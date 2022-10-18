9be54f217459c94789de2fdbec2ac646d6b3ad3c
Ilya12184 <volostnov.il@yandex.ru>
2022-10-18 17:32:57 +0300

    Добавлена задача 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+    if (a>b)
+{
+    Console.WriteLine($"Max = {a}");
+}
+    else
+{
+    Console.WriteLine($"Max = {b}");
+}
