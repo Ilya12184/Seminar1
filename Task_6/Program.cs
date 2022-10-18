00aef51b0d5d184c5e763d0eedcce316cf4cae02
Ilya12184 <volostnov.il@yandex.ru>
2022-10-18 18:00:20 +0300

    Добалена задача 6 на проверку четности

---
+++ Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if(a % 2 == 0)
+{
+  Console.WriteLine($"Число {a} является четным");  
+}
+else
+{
+   Console.WriteLine($"Число {a} является нечетным");
+}
