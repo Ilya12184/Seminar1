b1800bcfbfce4ee2afeee8e84cff5116b1ca085a
Ilya12184 <volostnov.il@yandex.ru>
2022-10-18 17:54:13 +0300

    Добавлена задача 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+
+if(b>max) max = b;
+if(c>max) max = c;
+
+Console.Write($"Max = {max}");
