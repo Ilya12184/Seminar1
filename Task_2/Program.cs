32b9c211f3f31b4a20313c7e94ef2720ffe1b5d3
Ilya12184 <volostnov.il@yandex.ru>
2022-10-18 18:20:25 +0300

    Добавлены комментарии к задачам

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,13 +1,16 @@
-﻿Console.Write("Введите первое число: ");
+﻿/*Напишите программу, которая на вход принимает два числа и выдаёт,
+какое число большее, а какое меньшее.*/
+
+Console.Write("Введите первое число: ");
 int a = int.Parse(Console.ReadLine()!);
 Console.Write("Введите второе число: ");
 int b = int.Parse(Console.ReadLine()!);
 
     if (a>b)
 {
-    Console.WriteLine($"Max = {a}");
+    Console.WriteLine($"Max = {a}, Min = {b}");
 }
     else
 {
-    Console.WriteLine($"Max = {b}");
+    Console.WriteLine($"Max = {b}, Min = {a}");
 }
--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.Write("Введите первое число: ");
+﻿/*Напишите программу, которая принимает на вход три числа
+и выдаёт максимальное из этих чисел.*/
+
+Console.Write("Введите первое число: ");
 int a = int.Parse(Console.ReadLine()!);
 Console.Write("Введите второе число: ");
 int b = int.Parse(Console.ReadLine()!);
